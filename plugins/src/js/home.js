3d21f73afda89bf0735390d353317051e4db62e2
vilrodriguez <vilmango@hotmail.com>
2020-12-21 04:40:26 -0500

    added plugins config for output css files and plugins

---
+++ plugins/src/js/home.js
@@ -0,0 +1,5 @@
+import '../css/index.css';
+
+//we cannot import the css like the previous line, with the webpack loader you'll be able to
+
+document.body.innerHTML='ORALES, ESTAMOS EN LA INTERNES!';
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
+// name of const is a CLASS
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
   entry: {
-    home: path.resolve('css-loader/src/js/home.js'),
-    contacts: path.resolve('css-loader/src/js/contacts.js'),
-    prices: path.resolve('css-loader/src/js/prices.js'),
+    home: path.resolve('plugins/src/js/home.js'),
+    contacts: path.resolve('plugins/src/js/contacts.js'),
+    prices: path.resolve('plugins/src/js/prices.js'),
     },
   output: {
-    path:path.resolve(__dirname, 'css-loader/dist'),
+    path:path.resolve(__dirname, 'plugins/dist'),
     filename: 'js/[name].js'
     },
 
@@ -18,12 +21,29 @@ module.exports = {
         //tell what type of file you will find, recieves regEx
         test: /\.css$/,
         //once it knows the type of file what do you awnt to do? use this loader:
-        use: ["style-loader",'css-loader']
+        use: [
+         {
+           loader: MiniCSSExtractPlugin.loader
+         },
+          'css-loader'
+        ]
       }
 
-    ]}
+    ]},
+    plugins: [
+      // es una clase, para instanciarla hay que usar NEW
+      new MiniCSSExtractPlugin({
+        filename: 'css/[name].css'
+      })
+    ]
 }
 
 // LOADERS
 // webpack functionanility that allows you to interpret .css files and others
 // CSS loader only loads the css but doesnt applies it, need to use style-loader
+
+// PLUGINS
+/*  para el css: mini-css-extract-plugin
+y el plugin para usar html en webpack: html-webpack-plugin
+
+ */
