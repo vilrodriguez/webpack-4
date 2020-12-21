75f3f9cf633b9e7920a4818c89e5e0880747533b
vilrodriguez <vilmango@hotmail.com>
2020-12-21 04:16:44 -0500

    added css loaders and styles

--- css-loader/src/js/home.js
+++ css-loader/src/js/home.js
@@ -2,4 +2,4 @@ import '../css/index.css';
 
 //we cannot import the css like the previous line, with the webpack loader you'll be able to
 
-alert('ORALES, ESTOY EN LA INTERNES');
+document.body.innerHTML='ORALES, ESTAMOS EN LA INTERNES!';
