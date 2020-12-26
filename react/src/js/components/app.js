d5bb0dcb6c9315839db629a5c60f4b2068dfda9a
vilrodriguez <vilmango@hotmail.com>
2020-12-25 23:08:32 -0500

    added a small app in React

--- react/src/js/components/app.js
+++ react/src/js/components/app.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Loader from './loader';
+// en webpack 4 los archivos de json no se necesitan loaders extras
+import data from './data.json';
 
+console.log(data);
 function App(){
+  const [loaderList, setLoaderList] = useState([]);
+  function handleClick(){
+    setLoaderList(data.loaders);
+  }
   return (
-    <div>Que linda app en React</div>
+    <div>
+      <ul>
+        {
+          loaderList.map((item)=>(
+          <Loader {...item} key={item.id}/>
+        ))
+        }
+      </ul>
+      <button onClick={handleClick}>Mostrar lo aprendido hasta ahorita</button>
+    </div>
   )
 }
 export default App;
